6d01f3ece84a8ccc127f5a1cf2f5008d39fd1061
junaid <jwantza021@student.wethinkcode.co.za>
2022-12-09 10:07:11 +0200

    Create new expense and find expense by person tests finished.

--- WeShareApiTests/Applications/WeShare/Steps/ExpensesSteps.cs
+++ WeShareApiTests/Applications/WeShare/Steps/ExpensesSteps.cs
@@ -15,12 +15,41 @@ namespace Applications.WeShare.Steps {
         private List<ExpenseDTO> _expensesList;
         private readonly ExpensesApi _expenseApi = new ExpensesApi(StepsHelper.BasePath);
 
-        [Step("Find expense with id <1>")]
+        [Step("Create new expense for personId <id> of description <desc>")]
+        public void CreateNewExpense(int id, string desc)
+        {
+            _expense = _expenseApi.CreateExpense(new NewExpenseDTO(id, "09/12/2022", desc, 100));
+
+        }
+
+        [Step("Verify expense for personId <id> with description <desc> was created")]
+        public void VerifyExpenseHasBeenFound(int id, string desc)
+        {
+            _expense.Should().NotBeNull();
+            _expense.PersonId.Should().Be(id);
+            _expense.Description.Should().Be(desc);
+        }
+
+
+
+        [Step("Find expense with id <id>")]
         public void FindExpenseWithId(int id)
         {
             _expense = _expenseApi.FindExpenseById(id);
 
         }
+
+        [Step("Find expenses for person with personId <id>")]
+        public void FindExpensesByPerson(int id)
+        {
+            _expensesList = _expenseApi.FindExpensesByPerson(id);
+
+        }
+
+
+
+
+        
 
         [Step("Verify expense has been found")]
         public void VerifyExpenseHasBeenFound()
--- WeShareApiTests/Applications/WeShare/specs/expenses.spec
+++ WeShareApiTests/Applications/WeShare/specs/expenses.spec
@@ -2,11 +2,23 @@
 
 tags: Expenses
 
+
+## Create new expense
+
+* Create new expense for personId "2" of description "WiFi"
+* Verify expense for personId "2" with description "WiFi" was created
+
+
 ## Find all expenses
 
 * Find all expenses
 * Verify expenses have been found
 
+
+## Find an expense by person ID
+
+* Find expenses for person with personId "1"
+* Verify expenses have been found
 
 
 ## Find an expense by ID
